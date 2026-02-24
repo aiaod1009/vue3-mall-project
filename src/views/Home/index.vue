3f113ba4e33dd6db3f77cff8963766f337d72f25
aiaod1009 <3449508132@qq.com>
2026-02-24 20:28:55 +0800

    home-面板组件封装

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -4,6 +4,7 @@ import HomeBanner from './components/HomeBanner.vue'
 import HomeNew from './components/HomeNew.vue'
 import HomeHot from './components/HomeHot.vue'
 import homeProduct from './components/HomeProduct.vue'
+import HomePanle from './components/HomePanle.vue'
 </script>
 
 <template>
@@ -14,4 +15,16 @@ import homeProduct from './components/HomeProduct.vue'
   <HomeNew />
   <HomeHot />
   <homeProduct />
+  <!-- 测试面板组件 -->
+  <HomePanle title="新鲜好物" subTitle="新鲜出炉 品质靠谱">
+    <div>
+      我是新鲜好物的插槽
+    </div>
+
+  </HomePanle>
+  <HomePanle title="人气推荐" subTitle="人气推荐  品质靠谱">
+    <div>
+      我是人气推荐的插槽
+    </div>
+  </HomePanle>
 </template>
