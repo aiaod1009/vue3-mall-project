380cf6b1c4a5d0261675be598dad59fe1770c6c0
aiaod1009 <3449508132@qq.com>
2026-02-24 18:05:09 +0800

    pinia优化导航数据

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-// 引入初始化样式文件
+// 引入初始化样式文件                                 
 import './styles/common.scss'
 
 import { createApp } from 'vue'
@@ -8,15 +8,15 @@ import App from './App.vue'
 import router from './router'
 
 // 测试接口函数
-import { getCategory } from '@/apis/test API'
+// import { getCategory } from '@/apis/test API'
 
-getCategory()
-  .then(res => {
-    console.log(res);
-  })
-  .catch(err => {
-    console.error("请求失败:", err);
-  })
+// getCategory()
+//   .then(res => {
+//     console.log(res);
+//   })
+//   .catch(err => {
+//     console.error("请求失败:", err);
+//   })
 const app = createApp(App)
 
 app.use(createPinia())
