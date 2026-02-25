6f00637664f7bbce9fb33cadbbaf1c7277f5542a
aiaod1009 <3449508132@qq.com>
2026-02-26 01:27:21 +0800

    详情页-全局组件插件化

--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -1,7 +1,5 @@
 <script setup>
 import DetailHot from './components/DetailHot.vue';
-import ImageView from '@/components/ImageView/index.vue';
-import XtxSku from '@/components/XtxSku/index.vue'
 import { getDetail } from '@/apis/detail';
 import { onMounted, ref } from 'vue';
 import { useRoute } from 'vue-router';
@@ -16,7 +14,7 @@ onMounted(() => getDetailData())
 
 // sku规格被操作时
 const skuChange = (sku) => {
-  console.log(sku)
+  // console.log(sku)
 }
 </script>
 
@@ -43,7 +41,7 @@ const skuChange = (sku) => {
           <div class="goods-info">
             <div class="media">
               <!-- 图片预览区 -->
-              <ImageView :image-list="goods.mainPictures" />
+              <XtxImageView :image-list="goods.mainPictures" />
               <!-- 统计数量 -->
               <ul class="goods-sales">
                 <li>
