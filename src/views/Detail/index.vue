4d298a22ee5b82bbaad49956e8de098de7d54469
aiaod1009 <3449508132@qq.com>
2026-02-25 23:57:07 +0800

    图片预览组件-props适配以及优化操作

--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -37,7 +37,7 @@ onMounted(() => getDetailData())
           <div class="goods-info">
             <div class="media">
               <!-- 图片预览区 -->
-              <ImageView />
+              <ImageView :image-list="goods.mainPictures" />
               <!-- 统计数量 -->
               <ul class="goods-sales">
                 <li>
