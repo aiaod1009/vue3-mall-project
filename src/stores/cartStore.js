4ba3df8f2a950eeeb1304a00fcae9bdbc3d1e070
aiaod1009 <3449508132@qq.com>
2026-02-27 15:04:00 +0800

    本地-列表购物车-单选功能实现

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -32,6 +32,13 @@ export const useCartStore = defineStore('cart', () => {
     cartList.value.splice(idx, 1)
   }
 
+  // 单选功能
+  const singleCheck = (skuId, selected) => {
+    // 通过skuId找到要修改的那一项 然后把它的selected修改为传过来的selected
+    const item = cartList.value.find((item) => item.skuId === skuId)
+    item.selected = selected
+  }
+
   // 计算属性
   // 1. 总的数量 所有项的count之和
   const allCount = computed(() => cartList.value.reduce((a, c) => a + c.count, 0))
@@ -43,7 +50,8 @@ export const useCartStore = defineStore('cart', () => {
     allCount,
     allPrice,
     addCart,
-    delCart
+    delCart,
+    singleCheck
   }
 }, {
   persist: true,
