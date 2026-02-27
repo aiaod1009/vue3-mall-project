45b5a6cb59778659ad61c0fbe50012b845c7178b
aiaod1009 <3449508132@qq.com>
2026-02-27 15:47:13 +0800

    本地-列表购物车-统计计算

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -52,6 +52,12 @@ export const useCartStore = defineStore('cart', () => {
   const allPrice = computed(() => cartList.value.reduce((a, c) => a + c.count * c.price, 0))
 
 
+  // 3. 已选择数量
+  const selectedCount = computed(() => cartList.value.filter(item => item.selected).reduce((a, c) => a + c.count, 0))
+  // 4. 已选择商品价钱合计
+  const selectedPrice = computed(() => cartList.value.filter(item => item.selected).reduce((a, c) => a + c.count * c.price, 0))
+  // 是否全选
+
   // 是否全选
   const isAll = computed(() => cartList.value.every((item) => item.selected))
   return {
@@ -59,6 +65,8 @@ export const useCartStore = defineStore('cart', () => {
     allCount,
     allPrice,
     isAll,
+    selectedCount,
+    selectedPrice,
     addCart,
     delCart,
     singleCheck,
